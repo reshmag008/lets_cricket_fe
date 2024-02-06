b79652d86358ce93406705d213c67b2373c7616f
Reshma <reshma.g@iinerds.com>
2024-02-06 15:14:54 +0530

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -17,9 +17,6 @@ import routes from "./routes";
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  // const createHistory = process.env.SERVER
-  //   ? createMemoryHistory
-  //   : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
   const createHistory = createWebHistory;
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -28,7 +25,6 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE),
   });
 
   return Router;
